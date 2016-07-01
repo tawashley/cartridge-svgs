f48f4279ccf9da17b78bc2c483ba23f03ac2f8fd
James Tudsbury <james.tudsbury1@gmail.com>
2016-07-01 17:16:38 +0100

    Chore: Remove comments from task.svgs.js. This is covered in the documentation and was too extensive to be inside the file.

--- _config/task.svgs.js
+++ _config/task.svgs.js
@@ -5,23 +5,23 @@
 function getTaskConfig(projectConfig) {
 	var taskConfig = {
 		svgSrc: projectConfig.paths.src.svgs + '**/*.svg', // Source of SVGs
-		outDir: '', // If using modes, leave this empty and configure on a mode level - determines the main output dir for all files when not configured individually
+		outDir: '',
 
 		settings: {
-		    "dest": "", // If using modes, leave this empty and configure on a mode level - determines where to put the SVG symbol/css sprite/def
+		    "dest": "",
 		    "mode": {
 		        "css": {
-		        	"dest": projectConfig.paths.src.sass + "/_objects", // where it specifically puts the css file (not where it'd put the def / symbol etc)
-		        	"sprite": process.cwd() + '/' + projectConfig.paths.dest.svgs + '/svgsprite.svg', // where it puts the svg file and what it names it
+		        	"dest": projectConfig.paths.src.sass + "/_objects",
+		        	"sprite": process.cwd() + '/' + projectConfig.paths.dest.svgs + '/svgsprite.svg',
 		            "render": {
-		            	"scss": { // make it render scss and not css
-		            		'dest': "_objects.svgsprite.scss" // scss file name
+		            	"scss": {
+		            		'dest': "_objects.svgsprite.scss"
 		            	}
 		            },
-		           	"layout": "packed", // default - layout of svg in the sprite
-		           	"prefix": ".svgsprite--%s", // custom - prefix for svg classes
-		           	"dimensions": "__dimensions", // custom - suffix for dimensions class e.g. .svgsprite--hamburger__dimensions
-		           	"bust": true // default - cache busting
+		           	"layout": "packed",
+		           	"prefix": ".svgsprite--%s",
+		           	"dimensions": "__dimensions",
+		           	"bust": true
 		        }
 	    	}
 	    }
