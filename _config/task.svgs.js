d006f759f4b66f517b81735668ac14e31e537bd9
tawashley <trisashley@gmail.com>
2016-09-15 12:22:53 +0100

    feat: Update svg sprite config

--- _config/task.svgs.js
+++ _config/task.svgs.js
@@ -2,33 +2,35 @@
 
 'use strict';
 
+var path = require('path');
+
 function getTaskConfig(projectConfig) {
-	var taskConfig = {
-		svgSrc: projectConfig.paths.src.svgs + '**/*.svg',
-		outDir: '',
+  var taskConfig = {
+    svgSrc: projectConfig.paths.src.svgs + '**/*.svg',
+    outDir: '',
 
-		settings: {
-		    "dest": "",
-		    "mode": {
-		        "css": {
-		        	"dest": projectConfig.paths.src.sass + "/_objects",
-		        	"sprite": process.cwd() + '/' + projectConfig.paths.dest.svgs + '/svgsprite.svg',
-		            "render": {
-		            	"scss": {
-		            		'dest': "_objects.svgsprite.scss"
-		            	}
-		            },
-		           	"layout": "packed",
-		           	"prefix": ".svgsprite--%s",
-		           	"dimensions": "__dimensions",
-		           	"bust": true
-		        }
-	    	}
-	    }
+    settings: {
+        "dest": "", // If using modes, leave this empty and configure on a mode level - determines where to put the SVG symbol/css sprite/def
+        "mode": {
+            "css": {
+              "dest": projectConfig.paths.src.sass + "/_tools", // where it specifically puts the css file (not where it'd put the def / symbol etc)
+              "sprite": process.cwd() + '/' + projectConfig.paths.dest.svgs + '/svg-sprite.svg', // where it puts the svg file and what it names it
+                "render": {
+                  "scss": { // make it render scss and not css
+                    'dest': "_tools.svg-sprite.scss", // generated scss sprite file
+                    'template': path.join(process.cwd(), projectConfig.paths.src.images, '/svg-sprite-sass.tpl')
+                  }
+                },
+               "layout": "packed", // default - layout of svg in the sprite
+               "prefix": ".svgsprite--%s", // custom - prefix for svg classes
+               "dimensions": "__dimensions", // custom - suffix for dimensions class e.g. .svg--hamburger__dimensions
+               "bust": false // default - cache busting
+            }
+        }
+      }
+  }
 
-	}
-
-	return taskConfig;
+  return taskConfig;
 }
 
 module.exports = getTaskConfig;
