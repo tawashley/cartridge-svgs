7eee31d758f59704d66088ae22fbd868f52ae716
tawashley <trisashley@gmail.com>
2016-09-15 12:30:39 +0100

    feat: Rename tpl file. Copy over tpl file on installation. Tweak to default svg config

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -11,15 +11,15 @@ var TASK_NAME = 'svgs';
 
 // Transform function for adding paths
 function projectConfigAddPaths(config) {
-	if(!config.paths.src.hasOwnProperty(TASK_NAME)) {
-		config.paths.src[TASK_NAME]  = config.dirs.src  + '/images/svgs/';
-	}
+  if(!config.paths.src.hasOwnProperty(TASK_NAME)) {
+    config.paths.src[TASK_NAME]  = config.dirs.src  + '/images/svgs/';
+  }
 
-	if(!config.paths.dest.hasOwnProperty(TASK_NAME)) {
-		config.paths.dest[TASK_NAME]  = config.dirs.dest  + '/images/svgs/';
-	}
+  if(!config.paths.dest.hasOwnProperty(TASK_NAME)) {
+    config.paths.dest[TASK_NAME]  = config.dirs.dest  + '/images/svgs/';
+  }
 
-	return config;
+  return config;
 }
 
 // Exit if NODE_ENV is development
@@ -28,10 +28,13 @@ cartridgeUtil.exitIfDevEnvironment();
 cartridgeUtil.ensureCartridgeExists();
 // Run through the project setup
 cartridgeUtil.addToRc()
-	.then(function() {
-		return cartridgeUtil.modifyProjectConfig(projectConfigAddPaths);
-	})
-	.then(function(){
-		return cartridgeUtil.addModuleConfig(path.resolve('_config', 'task.' + TASK_NAME + '.js'));
-	})
-	.then(cartridgeUtil.finishInstall);
+  .then(function() {
+    return cartridgeUtil.modifyProjectConfig(projectConfigAddPaths);
+  })
+  .then(function(){
+    return cartridgeUtil.addModuleConfig(path.resolve('_config', 'task.' + TASK_NAME + '.js'));
+  })
+  .then(function() {
+    cartridgeUtil.copyFileToProject('svg-sprite.tpl', path.join(config.dirs.src  + '/images/svgs'));
+  })
+  .then(cartridgeUtil.finishInstall);
